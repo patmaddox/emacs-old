5e9b2f02c7e7487c2c72f2b8f9bdc1008c85a014
Pat Maddox <pat.maddox@gmail.com>
2008-12-18 10:47:45 -0800

    nxhtml for sexy erb highlighting

--- padillac/ruby.el
+++ padillac/ruby.el
@@ -1,5 +1,15 @@
-; rhtml
-(setq auto-mode-alist (cons '("\\.html\\.erb" . nxml-mode) auto-mode-alist))
+;;; nxml (HTML ERB template support)
+(load "~/.emacs.d/vendor/nxhtml/autostart.el")
+
+(setq
+ nxhtml-global-minor-mode t
+ mumamo-chunk-coloring 'submode-colored
+ nxhtml-skip-welcome t
+ indent-region-mode t
+ rng-nxml-auto-validate-flag nil
+ nxml-degraded t)
+(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-nxhtml-mumamo))
+(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-nxhtml-mumamo))
 
 ; ruby
 (vendor 'ruby-hacks)
