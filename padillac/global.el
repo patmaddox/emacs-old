986c0619394237f8c22dc621d84b680e4e913b90
Pat Maddox <pat.maddox@gmail.com>
2008-12-17 16:00:49 -0800

    show parens, tab, server mode

--- padillac/global.el
+++ padillac/global.el
@@ -1,7 +1,18 @@
 ; Pimp my interface
 (add-to-list 'default-frame-alist '(font . "-apple-monaco-medium-r-normal--16-12
 0-72-72-m-120-iso10646-1"))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
 (transient-mark-mode t)
+(show-paren-mode t)
+
+(setq indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 2)
+(setq tab-width 2)
+(setq backward-delete-char-untabify nil)
+
 (autoload 'window-number-mode "window-number"
   "A global minor mode that enables selection of windows according to
   numbers with the C-x C-j prefix.  Another mode,
@@ -14,3 +25,5 @@ the mode-line."
   t)
 (window-number-mode 1)
 (window-number-meta-mode 1)
+
+(server-start)
