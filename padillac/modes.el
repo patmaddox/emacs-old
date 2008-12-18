ca2bae8726b76dc5033c4883da6ca369569a3485
Pat Maddox <pat.maddox@gmail.com>
2008-12-17 23:03:24 -0800

    js2 mode and some refactoring

---
+++ padillac/modes.el
@@ -0,0 +1,9 @@
+;; others
+(load "padillac/shell")
+(load "padillac/javascript")
+ 
+; magit
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")))
